380da6600ada0dc9be9c92c202d4d4f6524c0e39
wimi <331871902@qq.com>
2020-05-04 22:57:24 +0800

    feature：分离生产环境，开发环境配置

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,6 +12,7 @@ module.exports = {
     filename: 'bundle.js', // 输出文件名
     path: path.resolve(__dirname, 'dist'), //输出文件路径配置
   },
+  mode: 'development',
   module: {
     rules: [
       {
@@ -51,5 +52,8 @@ module.exports = {
   ],
   devServer: {
     contentBase: './dist', //默认服务于根目录下的index.html(可修改路径), 热加载是给开发人员使用的，它会开启服务器，将本地资源打包，监控到服务器中，给本地打包资源无关
+    open: true, //自动打开浏览器
+    port: 4000,
+    host: '0.0.0.0',
   },
 }
