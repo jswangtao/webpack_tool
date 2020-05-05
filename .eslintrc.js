984ec7dd82d11d63a12aa26c2f64fda2efdcc07a
wimi <331871902@qq.com>
2020-05-05 10:12:49 +0800

    feature：eslint配置修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,14 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "extends": "airbnb",
-  "env": {
-      "browser": true,
-      "node": true
+  parser: 'babel-eslint',
+  extends: 'airbnb',
+  env: {
+    browser: true,
+    node: true,
   },
-  "rules": {
-      "indent": ["error", 4],
-      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-      'react/jsx-indent': [
-        'error',
-        4
-    ],
-  }
-};
+  rules: {
+    indent: ['error', 4],
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-indent': ['error', 4],
+    'no-console': 'off',
+  },
+}
